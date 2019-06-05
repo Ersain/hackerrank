4d2f97567b94be286bd6b634daa20c53e4d24fdc
MagiskBoy <37865517+MagiskBoy@users.noreply.github.com>
2019-06-05 13:56:34 +0600

    Add PythonFunctionals section

---
+++ Solve Python/PythonFunctionals/MapAndLambda.py
@@ -0,0 +1,25 @@
+'''
+Problem: Map and Lambda Function.
+Description: Given an integer n.
+Print the fibonacci sequence until n,
+where each member of the sequence is x^3.
+E.g.
+5 -> [0, 1, 1, 8, 27]
+
+
+'''
+
+
+cube = lambda x: x*x*x
+
+
+def fibonacci(n):
+    fib = [0]*n
+
+    if n > 1:
+        fib[1] = 1
+
+        for i in range(2, n):
+            fib[i] = fib[i-1]+fib[i-2]
+
+    return fib
