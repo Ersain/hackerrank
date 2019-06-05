ffb76301d88dc680826f13912659d35f0d417ae9
MagiskBoy <37865517+MagiskBoy@users.noreply.github.com>
2019-06-05 13:34:09 +0600

    Add Date&Time section

---
+++ Solve Python/DateAndTime/CalendarModule.py
@@ -0,0 +1,12 @@
+'''
+Problem: Calendar Module.
+Description: Given a date,
+find what the day is on that date.
+E.g. 05/MAY/2019 == Wednesday.
+
+'''
+
+import calendar
+
+m, d, y = list(map(int, input().split()))
+print(list(calendar.day_name)[calendar.weekday(y, m, d)].upper())
