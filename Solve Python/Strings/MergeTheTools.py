421849ce14af420d46298302e75104e389bbf9db
MagiskBoy <37865517+MagiskBoy@users.noreply.github.com>
2019-06-05 23:15:31 +0600

    Add MergeTheTools problem

---
+++ Solve Python/Strings/MergeTheTools.py
@@ -0,0 +1,27 @@
+'''
+Problem: Merge the Tools!.
+Description: Given a string S and integer K,
+print len(S)/K strings without
+any subsequent occurrences.
+E.g.
+AABCAAADA, 3 ->
+AB, CA, AD
+
+'''
+
+
+def merge_the_tools(string, k):
+    temp = []
+    counter = 0
+    for i in range(len(string)):
+        counter += 1
+        if string[i] not in temp:
+            temp.append(string[i])
+        if counter == k:
+            print(''.join(temp))
+            temp = []
+            counter = 0
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
