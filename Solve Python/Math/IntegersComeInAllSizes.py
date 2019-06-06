9766b64ee90edda3442bc3fa983155019af52cfe
MagiskBoy <37865517+MagiskBoy@users.noreply.github.com>
2019-06-06 17:58:46 +0600

    Add Test Cases to Math

--- Solve Python/Math/IntegersComeInAllSizes.py
+++ Solve Python/Math/IntegersComeInAllSizes.py
@@ -7,8 +7,28 @@ Points: 10.
 '''
 
 
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
-print(a**b + c**d)
+def count(num_a, num_b, num_c, num_d):
+    return num_a**num_b + num_c**num_d
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+
+# print(count(a, b, c, d))
+
+
+def test_count_1():
+    assert count(1, 1, 1, 1) == 2
+    assert count(1, 5, 1, 6) == 2
+
+
+def test_count_2():
+    assert count(2, 2, 2, 2) == 8
+    assert count(2, 3, 2, 4) == 24
+
+
+def test_count_3():
+    assert count(3, 3, 3, 3) == 54
+    assert count(3, 4, 3, 4) == 162
--- Solve Python/Math/PolarCoordinates.py
+++ Solve Python/Math/PolarCoordinates.py
@@ -8,6 +8,30 @@ Points: 10.
 
 from cmath import phase
 
-s = complex(input())
-print(abs(s))
-print(phase(s))
+
+def count_abs(a, b):
+    return abs(complex(a, b))
+
+
+def count_phase(a, b):
+    return phase(complex(a, b))
+
+
+# s = complex(input())
+# print(abs(s))
+# print(phase(s))
+
+
+def test_abs():
+    assert count_abs(3, 4) == 5
+    assert count_abs(6, 8) == 10
+
+
+def test_phase_1():
+    assert count_phase(3, 4) > 0
+    assert count_phase(6, 8) > 0
+
+
+def test_phase_2():
+    assert count_phase(6, 0) == 0
+    assert count_phase(0, 8) > 1
