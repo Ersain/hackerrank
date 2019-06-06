e86a9392eff21dfea021716cc4717e289978b18e
MagiskBoy <37865517+MagiskBoy@users.noreply.github.com>
2019-06-06 19:03:06 +0600

    Add Test Cases to Strings

--- Solve Python/Strings/Mutations.py
+++ Solve Python/Strings/Mutations.py
@@ -7,10 +7,20 @@ Points: 10.
 
 def mutate_string(string, position, character):
     '''Attach a character at desired position in a string'''
-    return string[:position] + character + string[(position+1):]
+    return string[:position] + character + string[(position):]
 
-if __name__ == '__main__':
-    s = input()
-    i, c = input().split()
-    s_new = mutate_string(s, int(i), c)
-    print(s_new)
+# if __name__ == '__main__':
+#     s = input()
+#     i, c = input().split()
+#     s_new = mutate_string(s, int(i), c)
+#     print(s_new)
+
+
+def test_mutate_1():
+    assert mutate_string('London', 3, ', ') == 'Lon, don'
+
+def test_mutate_2():
+    assert mutate_string('123123123123', 6, ' ') == '123123 123123'
+
+def test_mutate_3():
+    assert mutate_string('!@#$%^', 5, '/') == '!@#$%/^'
--- Solve Python/Strings/StringSplitAndJoin.py
+++ Solve Python/Strings/StringSplitAndJoin.py
@@ -14,7 +14,18 @@ def split_and_join(line):
     '''
     return ('-'.join(line.split()))
 
-if __name__ == '__main__':
-    line = input()
-    result = split_and_join(line)
-    print(result)
+# if __name__ == '__main__':
+#     line = input()
+#     result = split_and_join(line)
+#     print(result)
+
+
+def test_split_join_1():
+    assert split_and_join('this is a string') == 'this-is-a-string'
+
+
+def test_split_join_2():
+    assert split_and_join('123 123') == '123-123'
+
+def test_split_join_3():
+    assert split_and_join('! @ # * ()') == '!-@-#-*-()'
